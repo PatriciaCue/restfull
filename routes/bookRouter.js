0312b74c5ffd7924c3d817df75a79333704887b6
Patricia Cuevas <patricia.cuevas@the-cocktail.com>
2022-09-27 09:38:24 +0200

    Añadir datos a la BD, metodo POST, colocar el codigo por carpetas, rutas, modulos

---
+++ routes/bookRouter.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+function routes(Book){
+const bookRouter = express.Router();
+
+bookRouter.route('/books')
+  .post((req,res)=>{
+    const book= new Book(req.body);
+    console.log(book);
+    book.save();
+    return res.status(201).json(book);
+  });
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    const { query } = req;
+    Book.find(query, (err,books)=>{
+      if(err){
+        return res.send(err);
+      }
+        return res.json(books);
+    });
+  });
+
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId,(err,books)=>{
+      if(err){
+        return res.send(err);
+      }
+        return res.json(books);
+    });
+});
+
+return bookRouter;
+}
+
+module.exports = routes;
